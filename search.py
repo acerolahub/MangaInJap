6476a83e5762c9d21810828654e7405000e2c09b
ralphsagbo <ralphsagbo2@gmail.com>
2021-08-16 20:35:13 +0200

    find all the images of a chapter

--- search.py
+++ search.py
@@ -4,8 +4,9 @@ from cmd import Cmd
 import re
 import requests
 import urllib.parse
+import tkinter
 from colorama import Fore, Back, Style
-
+from requests_html import HTMLSession
 
 class Manga:
     name = ""
@@ -17,6 +18,7 @@ class Manga:
         self.liste = liste
 
     def choice(self):
+        choice = []
         if self.liste == []:
             print(Fore.RED + "[-]" + Style.RESET_ALL, "Sorry, manga not found, you must try to add another source")
         elif len(self.liste) == 1:
@@ -33,6 +35,8 @@ class Manga:
                 print(Fore.RED + "[-]" + Style.RESET_ALL, "You must pick 1 or 2 number(s) and the number(s) must be between 0 and {}".format(len(self.liste)-1))
                 choice = input("Pick your poison ? (Y/n): ").split()
             print(Fore.GREEN + "[+]" + Style.RESET_ALL, "Nice, good choice: {}".format("-".join(choice)))
+        result = [ self.liste[int(i)] for i in choice]
+        return result
 
 
     def betweenrange(self, choice, size):
@@ -40,6 +44,36 @@ class Manga:
             if int(i) < 0 or int(i) >= size:
                 return False
         return True
+
+class Manga_one(Manga):
+    site = ""
+    name = ""
+    source = ""
+    nbchaps = 0
+    start = 0
+    chapter_format = ""
+
+    def __init__(self, liste):
+        Manga.__init__(self, liste[0][1], liste[0])
+        self.site = liste[0][0]
+        self.name = urllib.parse.urlparse(self.site).path.split('/')[-1]
+        self.source = urllib.parse.urlparse(self.site).hostname
+        self.nbchaps = eval(liste[0][2])
+        self.start = liste[0][3]
+        self.chapter_format = self.find_chapter_format()
+
+    def find_chapter_format(self):
+        if self.source == "mangahere.win":
+            self.chapter_format = MangaHere.chapter_format.replace("NAME", self.name)
+            print(self.name, self.chapter_format)
+
+
+    def display(self):
+        MangaHere.download(self.site, self.start)
+        top = tkinter.Tk()
+        pass
+    
+        
 
 class Source:
     _name = ""
@@ -80,18 +114,25 @@ class Terminal(Cmd):
         print(".: quit")
 
     def do_search(self, args):
+        # Maybe check the history here at first
         source = MangaHere("http://mangahere.win/")
         manga = Manga(args, source.find(args))
-        manga.choice()
+        result = manga.choice()
+        if len(result) == 1:
+            manga_one = Manga_one(result)
+            manga_one.display()
 
     def do_quit(self, args):
         exit(0)
 
 class MangaHere(Source):
+    chapter_format = "http://mangahere.win/NAME-chapter-NUM"
+
     def __init__(self, site):
         Source.__init__(self, site)
         self.searchpath = urllib.parse.urljoin(self._site, "search?q=")
         self.q = "q"
+
 
     def find(self, name):
         names = []
@@ -113,13 +154,15 @@ class MangaHere(Source):
                     names.extend(result)
 
 
-            for i in names:
-                #pass
-                print(i)
 
             
         except requests.exceptions.RequestException as err:
             print(Fore.RED + "[-]" + Style.RESET_ALL, "Host {} not found".format(self._site))
+
+        names = [i + (0,) for i in names]
+
+        for i in names:
+            print(i)
 
         return names
 
@@ -129,8 +172,35 @@ class MangaHere(Source):
         return names
 
 
+    @staticmethod
+    def download(site, num):
+        if num == 0:
+            site = site.replace("-chapter-NUM", "")
+            response = requests.get(site)
+            search_re = r'<span><a  href="(.*?)"\s*?.*?class="chapter_title"> : (.*?) </p></span>'
+            names = re.findall(search_re, response.text, re.DOTALL)
+            site = names[-1][0]
+        else:
+            site = site.replace("NUM", num)
+        session = HTMLSession()
+        response = session.get(site)
+        response.html.render()
+        search_re = r'id="arraydata" style="display:none">(.*?)</p>'
+        names = re.findall(search_re, response.html.html, re.DOTALL)[0]
+        names = names.split(',')
+        for i in names:
+            print(i)
+        #print(response.html.html)
+
+                                #<a class="loadAllImgPage pull-right next hide" href="http://mangahere.win/bleach-digital-colored-comics-chapter-2">
+
 term = Terminal()
-term.cmdloop()
+#term.cmdloop()
+term.do_search("bleach")
 
 
-# prochaine étape, parcourir toutes les pages s'il y en a plusieurs et enregistrer les liens et sites
+"""
+prochaine étape, télécharger et enregistrer les différents liens
+https://www.activestate.com/resources/quick-reads/how-to-add-images-in-tkinter/
+https://theautomatic.net/2019/01/19/scraping-data-from-javascript-webpage-python/
+"""
